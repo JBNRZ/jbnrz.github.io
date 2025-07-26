177187952610a9ed373574d9a80f84fcdb7db166
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-07-26 22:58:21 +0800

    update

--- cbctf/assets/settings-Bt2XPTU0.js
+++
@@ -1,1 +0,0 @@
-import{e,u as a,r as n}from"./index-C2v50R19.js";function r(){const{id:t}=e(),s=a();return n.useEffect(()=>{s(`/admin/contests/${t}`)},[t,s]),null}export{r as default};
---
+++ cbctf/assets/settings-yE3QZvwY.js
@@ -0,0 +1,1 @@
+import{e,u as a,r as n}from"./index-DOX0JLyq.js";function r(){const{id:t}=e(),s=a();return n.useEffect(()=>{s(`/admin/contests/${t}`)},[t,s]),null}export{r as default};
